04289c1e8dbcb208b9315d9498f1b55a7c7c5328
SamAvaAth <bhuvneshkaushik@hotmail.com>
2024-11-07 16:11:24 +1100

    Update game.js

--- game.js
+++ game.js
@@ -2,9 +2,6 @@ let score = 0;
 let question, correctAnswer;
 let userAnswer = '';
 let submitButtonX, submitButtonY, submitButtonWidth = 100, submitButtonHeight = 30;
-let columns = [];
-let fontSize = 20;
-let matrixStream = [];
 
 // Generate initial question
 function generateQuestion() {
@@ -16,13 +13,6 @@ function generateQuestion() {
     correctAnswer = operation === '+' ? num1 + num2 : num1 - num2;
 }
 
-// Matrix background setup
-function setupMatrix() {
-    for (let i = 0; i < width / fontSize; i++) {
-        matrixStream.push(new Stream(i * fontSize));
-    }
-}
-
 function setup() {
     createCanvas(400, 600);
     textFont('monospace');
@@ -30,20 +20,13 @@ function setup() {
     submitButtonX = width / 2 - submitButtonWidth / 2;
     submitButtonY = height - 80;
     generateQuestion();
-    setupMatrix();
 }
 
 function draw() {
-    background(0, 150); // Semi-transparent background for matrix effect
-
-    // Draw matrix background effect
-    for (let stream of matrixStream) {
-        stream.update();
-        stream.render();
-    }
+    background(135, 206, 235); // Light blue background
 
     // Display the question and current score
-    fill(255);
+    fill(0);
     textAlign(CENTER, CENTER);
     textSize(24);
     text(`Score: ${score}`, width / 2, 50);
@@ -92,62 +75,4 @@ function checkAnswer() {
     }
     userAnswer = '';
     generateQuestion();
-}
-
-// Class for a single falling number in the matrix effect
-class Symbol {
-    constructor(x, y, speed) {
-        this.x = x;
-        this.y = y;
-        this.speed = speed;
-        this.value;
-        this.switchInterval = round(random(2, 20));
-    }
-
-    setToRandomSymbol() {
-        if (frameCount % this.switchInterval === 0) {
-            this.value = String(floor(random(0, 10)));
-        }
-    }
-
-    rain() {
-        this.y = (this.y >= height) ? 0 : this.y + this.speed;
-    }
-}
-
-// Class for streams of symbols in the matrix effect
-class Stream {
-    constructor(x) {
-        this.symbols = [];
-        this.totalSymbols = round(random(5, 30));
-        this.speed = random(2, 5);
-        this.x = x;
-        this.init();
-    }
-
-    init() {
-        let y = random(-1000, 0);
-        for (let i = 0; i < this.totalSymbols; i++) {
-            let symbol = new Symbol(this.x, y, this.speed);
-            symbol.setToRandomSymbol();
-            this.symbols.push(symbol);
-            y -= fontSize;
-        }
-    }
-
-    render() {
-        for (let i = 0; i < this.symbols.length; i++) {
-            fill(0, 255, 70);
-            text(this.symbols[i].value, this.symbols[i].x, this.symbols[i].y);
-            this.symbols[i].rain();
-            this.symbols[i].setToRandomSymbol();
-        }
-    }
-
-    update() {
-        for (let symbol of this.symbols) {
-            symbol.rain();
-            symbol.setToRandomSymbol();
-        }
-    }
 }
